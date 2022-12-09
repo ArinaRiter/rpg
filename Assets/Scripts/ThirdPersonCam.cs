e4fdc69e80e5d8181146d06c4618950d116629a1
annagrrrr <117434839+annagrrrr@users.noreply.github.com>
2022-12-09 12:40:53 +0300

    не ьей
    все плохо

---
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+    private void Update()
+    {
+        Vector3 viewDir=player.position - new Vector3(transform.position.x,transform.position.y,transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 inputDir = orientation.forward * verticalInput * orientation.right * horizontalInput;
+
+    }
+}
