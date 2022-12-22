07fc1c599268139a48e3dfe501ad08493a4a75b9
annagrrrr <117434839+annagrrrr@users.noreply.github.com>
2022-12-22 22:30:14 +0300

    ogo ura ne opyat' a snova

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Character : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
     private CharacterController characterController;
 
+    private float ySpeed; //a
+    private float originalStepOffset;
+
     public float Speed = 5f;
     public float JumpForce = 1f;
+    public float jumpSpeed; //a
 
     [SerializeField] float rotationSmoothTime;
     [SerializeField] private Camera _camera;
@@ -17,6 +21,7 @@ public class Character : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
 
     void FixedUpdate()
@@ -25,8 +30,25 @@ public class Character : MonoBehaviour
         Vector3 moveDirection = _camera.transform.TransformDirection(move);
         move = moveDirection;
 
-        characterController.Move(move * Speed);
-        if(move.magnitude>=0.1f)
+        ySpeed += Physics.gravity.y * Time.deltaTime;//a
+        if (characterController.isGrounded)//a
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;//a
+            if (Input.GetButtonDown("Jump"))//a
+            {
+                ySpeed = jumpSpeed;
+            }
+        }
+        else//a
+        {
+            characterController.stepOffset = 0;
+        }
+
+        Vector3 velocity = move * Speed;//a
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
+        if (move.magnitude>=0.1f)
         {
             float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
             currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentAngleVelocity, rotationSmoothTime);
