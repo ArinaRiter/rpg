d8ff4f16bc4b8c4f29cac290c6f26a3aaf90f7bc
annagrrrr <117434839+annagrrrr@users.noreply.github.com>
2022-12-30 20:46:28 +0300

    INVENTORY SYSTEM ALMOST GOTOVA

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Item", menuName ="Item/Create New Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public int value;
+    public string description;
+    public Sprite icon;
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+    public Item(int id, string itemName, string description, /*Sprite icon,*/ Dictionary<string, int> stats)
+    {
+        this.id = id;
+        this.itemName = itemName;
+        this.description = description;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + itemName);
+        this.stats = stats;
+    }
+    public Item(Item item)
+    {
+        this.id = item.id;
+        this.itemName = item.itemName;
+        this.description = item.description;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + item.itemName);
+        this.stats = item.stats;
+    }
+}
